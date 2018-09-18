e340f8ec6bc09352b7627123649178dda4c91832
Anuj Agarwal <anuj.ism@gmail.com>
2018-09-18 18:32:12 +0530

    Added configuration for celery flower

--- {{cookiecutter.repostory_name}}/app/src/{{cookiecutter.django_project_name}}/{{cookiecutter.django_default_app_name}}/tasks.py
+++ {{cookiecutter.repostory_name}}/app/src/{{cookiecutter.django_project_name}}/{{cookiecutter.django_default_app_name}}/tasks.py
@@ -1,5 +1,15 @@
+{% if cookiecutter.use_celery == 'y' %}
 from celery.utils.log import get_task_logger
 
 from {{cookiecutter.django_project_name}}.celery import app
 
 logger = get_task_logger(__name__)
+
+
+# Sample Task - You can remove it. This is just for an example.
+
+@app.task
+def add(x, y):
+    return x + y
+
+{% endif %}
