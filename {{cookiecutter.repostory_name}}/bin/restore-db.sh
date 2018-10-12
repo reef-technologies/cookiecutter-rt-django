8fdce47f6593038eb9ab08a2d4c67af822da3616
Pawel Polewicz <p.polewicz@gmail.com>
2018-10-12 06:00:10 +0200

    Improve restore script

--- {{cookiecutter.repostory_name}}/bin/restore-db.sh
+++ {{cookiecutter.repostory_name}}/bin/restore-db.sh
@@ -1,12 +1,10 @@
-#!/bin/bash
+#!/bin/bash -eu
 if [ "$(basename "$0")" == 'bin' ]; then
-  cd ..
+  . ../.envrc
+else
+  . .envrc
 fi
-
-. .envrc
 
 target="$1"
 
 gunzip < "$target" | docker exec -i {{cookiecutter.django_project_name}}_db_1 psql -U postgres
-
-echo "$target"
