23f87c06e49c2d81619001a9954f838374280631
Maciej Lech <maciej.lech@reef.pl>
2020-08-31 21:39:21 +0200

    Set label for base-image

--- {{cookiecutter.repostory_name}}/deploy.sh
+++ {{cookiecutter.repostory_name}}/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -eux
 # Copyright 2020, Reef Technologies (reef.pl), All rights reserved.
 
-# Build and tag the first image from multi-stage app Dockerfile
-# to mark it as not dangling
-docker build -t {{cookiecutter.django_project_name}}/app-build --target base-image -f app/Dockerfile  app
-
 docker-compose build
+
+# Tag the first image from multi-stage app Dockerfile to mark it as not dangling
+BASE_IMAGE=$(docker images --quiet --filter="label=builder=true" | head -n1)
+docker image tag ${BASE_IMAGE} {{cookiecutter.django_project_name}}/app-builder
 
 SERVICES=$(docker-compose ps --services 2>&1 > /dev/stderr \
            | grep -v -e 'is not set' -e nginx -e db -e redis)
