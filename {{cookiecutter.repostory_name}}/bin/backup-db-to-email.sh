fd7b685247ec23b73dc1a8f73481f6ff7005fdd9
Maciej Lech <maciej.lech@reef.pl>
2020-11-23 15:11:32 +0100

    Update after PR

--- {{cookiecutter.repostory_name}}/bin/backup-db-to-email.sh
+++ {{cookiecutter.repostory_name}}/bin/backup-db-to-email.sh
@@ -2,13 +2,14 @@
 
 if [ "$(basename "$0")" == 'bin' ]; then
   cd ..
-  . bin/backup-db.sh
-else
-  . bin/backup-db.sh
 fi
+
+. bin/backup-db.sh
 
 date
 
 EMAIL_CREDS="${EMAIL_HOST_USER}:${EMAIL_HOST_PASSWORD}@${EMAIL_HOST}:${EMAIL_PORT}" bin/emailhelper.py --to "$1" --subject "Backup of ${POSTGRES_DB}" -f "$target"
 
 echo "Email sent successfully"
+
+rm "$target"
