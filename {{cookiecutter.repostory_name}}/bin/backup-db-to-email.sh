8dc6f9d45228449e99097c12f5ab6ac75d322d85
Maciej Szumocki <75223366+mszumocki-reef@users.noreply.github.com>
2021-01-07 19:44:44 +0100

    Apply suggestions from code review
    Co-authored-by: Paweł Polewicz <p.polewicz@gmail.com>

--- {{cookiecutter.repostory_name}}/bin/backup-db-to-email.sh
+++ {{cookiecutter.repostory_name}}/bin/backup-db-to-email.sh
@@ -4,7 +4,7 @@ if [ "$(basename "$0")" == 'bin' ]; then
   cd ..
 fi
 
-if [ ! -e "$1" ]; then
+if [ ! -f "$1" ]; then
   echo "Pass existing backup file name (with .backups/ directory name) as the first argument"
   exit 127
 fi
@@ -16,4 +16,3 @@ date
 EMAIL_CREDS="${EMAIL_HOST_USER}:${EMAIL_HOST_PASSWORD}@${EMAIL_HOST}:${EMAIL_PORT}" bin/emailhelper.py --to "${EMAIL_TARGET}" --subject "Backup of ${POSTGRES_DB}" -f "$1"
 
 echo "Email sent successfully"
-
--- {{cookiecutter.repostory_name}}/tools/backup-b2/send_backup.sh
+++ {{cookiecutter.repostory_name}}/tools/backup-b2/send_backup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 b2 authorize-account "$BACKUP_B2_KEY_ID" "$BACKUP_B2_KEY_SECRET"
-b2 upload-file "$BACKUP_B2_BUCKET" "$1" $(basename "$1")
+b2 upload-file "$BACKUP_B2_BUCKET" "$1" "$(basename "$1")"
