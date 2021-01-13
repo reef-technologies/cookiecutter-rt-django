85d91fe1ae49979696da3402d935d5f736c921c2
Maciej Szumocki <maciej.szumocki@reef.pl>
2021-01-13 13:58:35 +0100

    Fail fast for B2 scripts

--- {{cookiecutter.repostory_name}}/tools/backup-b2/retrieve_backup.sh
+++ {{cookiecutter.repostory_name}}/tools/backup-b2/retrieve_backup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-b2 authorize-account "$BACKUP_B2_KEY_ID" "$BACKUP_B2_KEY_SECRET"
-b2 download-file-by-name "$BACKUP_B2_BUCKET" "$1" ".backups/$(basename "$1")"
+b2 authorize-account "$BACKUP_B2_KEY_ID" "$BACKUP_B2_KEY_SECRET" && \
+  b2 download-file-by-name "$BACKUP_B2_BUCKET" "$1" ".backups/$(basename "$1")"
 
