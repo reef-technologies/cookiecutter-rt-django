e13a93ed975016042412623b929b750a70862ce8
Aleksandr Goncharov <aleksandr.goncharov@reef.pl>
2021-08-11 15:04:27 +0300

    Add ENV_FILL_MISSING_VALUES description

--- {{cookiecutter.repostory_name}}/app/src/{{cookiecutter.django_project_name}}/settings.py
+++ {{cookiecutter.repostory_name}}/app/src/{{cookiecutter.django_project_name}}/settings.py
@@ -20,6 +20,12 @@ root = environ.Path(__file__) - 2
 
 env = environ.Env(DEBUG=(bool, False))
 
+# .env file contents are not passed to docker image during build stage;
+# this results in errors if you require some env var to be set, as if in "env('MYVAR')" -
+# obviously it's not set during build stage, but you don't care and want to ignore that.
+# To mitigate this, we set ENV_FILL_MISSING_VALUES=1 during build phase, and it activates
+# monkey-patching of "environ" module, so that all unset variables are set to None and
+# the library is not complaining anymore
 if env.bool('ENV_FILL_MISSING_VALUES', default=False):
 
     def patch(fn):
