d3629de3da57ba28d8a2f675e070488b693a1fb5
Aleksandr Goncharov <aleksandr.goncharov@reef.pl>
2021-09-08 22:15:47 +0300

    Fix celery-worker logs

--- {{cookiecutter.repostory_name}}/app/envs/prod/celery-entrypoint.sh
+++ {{cookiecutter.repostory_name}}/app/envs/prod/celery-entrypoint.sh
@@ -4,7 +4,7 @@ set -eu
 # below we define two workers types (each may have any concurrency);
 # each worker may have its own settings
 WORKERS="master worker"
-OPTIONS="-A {{cookiecutter.django_project_name}} -E -l ERROR --pidfile=/var/run/celery/%n.pid --logfile=-"
+OPTIONS="-A {{cookiecutter.django_project_name}} -E -l ERROR --pidfile=/var/run/celery-%n.pid --logfile=/var/log/celery-%n.log"
 
 # set up settings for workers and run the latter;
 # here events from "celery" queue (default one, will be used if queue not specified)
@@ -15,4 +15,4 @@ celery multi start $WORKERS $OPTIONS \
     -Q:worker worker --autoscale:worker=$CELERY_WORKER_CONCURRENCY,0
 
 trap "celery multi stop $WORKERS $OPTIONS; exit 0" SIGINT SIGTERM
-sleep infinity
+tail -f /var/log/celery-*.log
