3f7e5bdf4f394ddd611bf34e5fd677bdad00e656
Aleksandr Goncharov <aleksandr.goncharov@reef.pl>
2021-10-04 23:50:10 +0300

    Wait for database up to 10s

--- {{cookiecutter.repostory_name}}/app/envs/prod/entrypoint.sh
+++ {{cookiecutter.repostory_name}}/app/envs/prod/entrypoint.sh
@@ -2,6 +2,6 @@
 
 # We assume that WORKDIR is defined in Dockerfile
 
-./manage.py wait_for_database
+./manage.py wait_for_database --timeout 10
 
 gunicorn --workers=4 --bind=0.0.0.0:8000 {{ cookiecutter.django_project_name }}.wsgi:application --access-logfile=-
