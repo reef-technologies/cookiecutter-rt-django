39fa74d1c5ff4d739698c3101ee550cd5735e019
Maciej Szumocki <maciej.szumocki@reef.pl>
2021-10-05 13:58:02 +0200

    Run celery processes with nice

--- {{cookiecutter.repostory_name}}/app/envs/prod/celery-entrypoint.sh
+++ {{cookiecutter.repostory_name}}/app/envs/prod/celery-entrypoint.sh
@@ -10,7 +10,7 @@ OPTIONS="-A {{cookiecutter.django_project_name}} -E -l ERROR --pidfile=/var/run/
 # here events from "celery" queue (default one, will be used if queue not specified)
 # will go to "master" workers, and events from "worker" queue go to "worker" workers;
 # by default there are no workers, but each type of worker may scale up to 4 processes
-celery multi start $WORKERS $OPTIONS \
+nice celery multi start $WORKERS $OPTIONS \
     -Q:master celery --autoscale:master=$CELERY_MASTER_CONCURRENCY,0 \
     -Q:worker worker --autoscale:worker=$CELERY_WORKER_CONCURRENCY,0
 
