75659101f065997b6648377539c85f17d64e4c18
awierzbicki-reef <97038302+awierzbicki-reef@users.noreply.github.com>
2022-01-08 15:55:38 +0100

    Install AWS CLI and jq tool in prepare-os.sh
    They're needed to use custom Python package index hosted on AWS.

--- {{cookiecutter.repostory_name}}/bin/prepare-os.sh
+++ {{cookiecutter.repostory_name}}/bin/prepare-os.sh
@@ -5,11 +5,24 @@ DOCKER_BIN="$(command -v docker || true)"
 DOCKER_COMPOSE_BIN="$(command -v docker-compose || true)"
 SENTRY_CLI="$(command -v sentry-cli || true)"
 B2_CLI="$(command -v b2 || true)"
+AWS_CLI="$(command -v aws || true)"
+JQ_BIN="$(command -v jq || true)"
 
-if [ -x "${DOCKER_BIN}" ] && [ -x "${DOCKER_COMPOSE_BIN}" ] && [ -x "${SENTRY_CLI}" ] && [ -x "${B2_CLI}" ]; then
+if [ -x "${DOCKER_BIN}" ] && [ -x "${DOCKER_COMPOSE_BIN}" ] && [ -x "${SENTRY_CLI}" ] && [ -x "${B2_CLI}" ] && [ -x "${AWS_CLI}" ] && [ -x "${JQ_BIN}" ]; then
     echo "\e[31mEverything required is already installed!\e[0m";
     exit 1;
 fi
+
+WORK_DIR="$(mktemp -d)"
+if [ ! "${WORK_DIR}" ] || [ ! -d "${WORK_DIR}" ]; then
+  echo "Could not create temp dir"
+  exit 1
+fi
+cd "${WORK_DIR}"
+cleanup() {
+  rm -rf "${WORK_DIR}"
+}
+trap cleanup EXIT
 
 DEBIAN_FRONTEND=noninteractive
 
@@ -34,4 +47,15 @@ fi
 
 if [ ! -x "${DOCKER_COMPOSE_BIN}" ]; then
     apt-get -y install docker-ce docker-compose
+fi
+
+if [ ! -x "${AWS_CLI}" ]; then
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+    unzip awscliv2.zip
+    ./aws/install
+fi
+
+if [ ! -x "${JQ_BIN}" ]; then
+  curl -s --output /usr/local/bin/jq -L https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
+  chmod a+x /usr/local/bin/jq
 fi
