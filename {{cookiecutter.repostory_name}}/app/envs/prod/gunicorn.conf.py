ad5786e9f0509d585918fd0272c0c87eea3fec3c
Michał Nowacki <77497496+mpnowacki-reef@users.noreply.github.com>
2022-03-21 10:31:36 +0100

    Update {{cookiecutter.repostory_name}}/app/envs/prod/gunicorn.conf.py
    Co-authored-by: Paweł Polewicz <p.polewicz@gmail.com>

--- {{cookiecutter.repostory_name}}/app/envs/prod/gunicorn.conf.py
+++ {{cookiecutter.repostory_name}}/app/envs/prod/gunicorn.conf.py
@@ -1,8 +1,9 @@
+import multiprocessing
 {% if cookiecutter.monitoring == "y" %}
 from prometheus_client import multiprocess
 
 {% endif %}
-workers = 4
+workers = 2 * multiprocessing.cpu_count() + 1
 bind = '0.0.0.0:8000'
 wsgi_app = '{{ cookiecutter.django_project_name }}.wsgi:application'
 access_logfile = '-'
