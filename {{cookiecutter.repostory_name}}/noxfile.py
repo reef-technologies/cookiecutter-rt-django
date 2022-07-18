eab5a87730d45d0a69f9457a992ee49a9b4fe379
Aleksandr Goncharov <aleksandr.goncharov@reef.pl>
2022-07-18 12:35:24 +0300

    Add noxfile and basic config files

---
+++ {{cookiecutter.repostory_name}}/noxfile.py
@@ -0,0 +1,57 @@
+import nox
+from pathlib import Path
+
+
+ROOT = Path('.')
+PYTHON_VERSIONS = ['3.9']
+APP_ROOT = ROOT / 'app' / 'src'
+
+nox.options.default_venv_backend = 'venv'
+nox.options.stop_on_first_error = True
+nox.options.reuse_existing_virtualenvs = True
+
+
+@nox.session(python=PYTHON_VERSIONS)
+def lint(session):
+    session.install('flake8')
+    with session.chdir(str(APP_ROOT)):
+        session.run('flake8', '--ignore', 'E501', '.')
+
+
+@nox.session(python=PYTHON_VERSIONS)
+def type_check(session):
+    with session.chdir(str(APP_ROOT)):
+        session.install(
+            '-r', 'requirements.txt',
+            'mypy',
+            'django-stubs[compatible-mypy]',
+            'types-requests',
+            'types-python-dateutil',
+            'djangorestframework-stubs[compatible-mypy]',
+        )
+        session.run('mypy', '.')
+
+
+@nox.session(python=PYTHON_VERSIONS)
+def security_check(session):
+    session.install('bandit')
+    with session.chdir(str(APP_ROOT)):
+        session.run('bandit', '--ini', 'bandit.ini', '-r', '.')
+
+
+@nox.session(python=PYTHON_VERSIONS)
+def test(session):
+    with session.chdir(str(APP_ROOT)):
+        session.install(
+            '-r', 'requirements.txt',
+            'pytest',
+            'pytest-django',
+            'ipdb',
+            'freezegun',
+        )
+        session.run(
+            'pytest',
+            '-W', 'ignore::DeprecationWarning', '-s', '-x', '-vv',
+            'project',
+            *session.posargs,
+        )
