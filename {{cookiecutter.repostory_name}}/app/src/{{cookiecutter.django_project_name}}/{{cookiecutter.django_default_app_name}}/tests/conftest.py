fd97f30c2634a3c28ab987302228ea715f7ef1da
Aleksandr Goncharov <aleksandr.goncharov@reef.pl>
2022-07-21 22:18:22 +0300

    Add tests example

---
+++ {{cookiecutter.repostory_name}}/app/src/{{cookiecutter.django_project_name}}/{{cookiecutter.django_default_app_name}}/tests/conftest.py
@@ -0,0 +1,8 @@
+import pytest
+
+
+# @pytest.fixture
+# def some() -> int:
+#     # setup code
+#     yield 1
+#     # teardown code
---
+++ {{cookiecutter.repostory_name}}/app/src/{{cookiecutter.django_project_name}}/{{cookiecutter.django_default_app_name}}/tests/test.py
@@ -0,0 +1,13 @@
+# from datetime import timedelta
+
+# import pytest
+# from django.utils.timezone import now
+# from freezegun import freeze_time
+
+
+# def test_some(some):
+#     with freeze_time(now() - timedelta(days=1)):
+#         assert some == 1
+
+#     with pytest.raises(ZeroDivisionError):
+#         1 / 0
