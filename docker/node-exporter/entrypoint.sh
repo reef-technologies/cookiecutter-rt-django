a09100e0463cd0d59306cbeaf2318cc8c53decc7
Michał Nowacki <michal.nowacki@reef.pl>
2022-08-23 12:47:02 +0200

    fix setting instance type to UNKNOWN

--- docker/node-exporter/entrypoint.sh
+++ docker/node-exporter/entrypoint.sh
@@ -2,12 +2,13 @@
 
 set -x
 
-INSTANCE_TYPE_EC2=`wget http://169.254.169.254/latest/meta-data/instance-type -O- --timeout=5`
-INSTANCE_TYPE="$(INSTANCE_TYPE_EC2:-UNKNOWN)"
+INSTANCE_TYPE=`wget http://169.254.169.254/latest/meta-data/instance-type -O- --timeout=5`
+[ -z "$INSTANCE_TYPE" ] && INSTANCE_TYPE='UNKNOWN'
+
 cat << EOF > /home/nobody/textfile_collector_metrics/instance_type.prom
 # HELP node_aws_ec2_instance_type type of ec2 instance
 # TYPE node_aws_ec2_instance_type gauge
 node_aws_ec2_instance_type{instance_type="$INSTANCE_TYPE"} 1
 EOF
 
-/bin/node_exporter --collector.textfile.directory=/home/nobody/textfile_collector_metrics/ "$@"
+exec /bin/node_exporter --collector.textfile.directory=/home/nobody/textfile_collector_metrics/ "$@"
