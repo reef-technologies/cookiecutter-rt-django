24d1c1f7315e8de622c34d59aa0d41fd6b42d642
Michał Nowacki <77497496+mpnowacki-reef@users.noreply.github.com>
2022-08-23 11:30:28 +0200

    Update docker/node-exporter/entrypoint.sh
    Co-authored-by: Paweł Polewicz <p.polewicz@gmail.com>

--- docker/node-exporter/entrypoint.sh
+++ docker/node-exporter/entrypoint.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-set -e
 set -x
 
-INSTANCE_TYPE=`wget http://169.254.169.254/latest/meta-data/instance-type -O- --timeout=5`
-
+INSTANCE_TYPE_EC2=`wget http://169.254.169.254/latest/meta-data/instance-type -O- --timeout=5`
+INSTANCE_TYPE="$(INSTANCE_TYPE_EC2:-UNKNOWN)"
 cat << EOF > /home/nobody/textfile_collector_metrics/instance_type.prom
 # HELP node_aws_ec2_instance_type type of ec2 instance
 # TYPE node_aws_ec2_instance_type gauge
