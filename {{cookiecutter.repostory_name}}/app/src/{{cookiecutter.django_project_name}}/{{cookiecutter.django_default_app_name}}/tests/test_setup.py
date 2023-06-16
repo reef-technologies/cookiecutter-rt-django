fa5bb1794998ec8132a69e1e4479f42435770af7
Aleksandr Goncharov <aleksandr.goncharov@reef.pl>
2023-06-16 10:36:34 +0300

    Fix minor tests issues

--- {{cookiecutter.repostory_name}}/app/src/{{cookiecutter.django_project_name}}/{{cookiecutter.django_default_app_name}}/tests/test_setup.py
+++ {{cookiecutter.repostory_name}}/app/src/{{cookiecutter.django_project_name}}/{{cookiecutter.django_default_app_name}}/tests/test_setup.py
@@ -1,11 +1,15 @@
+"""
+This test file is here always to indicate that everything was installed and the CI was able to run tests.
+It should always pass as long as all dependencies are properly installed.
+"""
 from datetime import timedelta
-
-import pytest
 from django.utils.timezone import now
 from freezegun import freeze_time
 
+import pytest
 
-def test_some(db, some):
+
+def test__setup(db, some):
     with freeze_time(now() - timedelta(days=1)):
         assert some == 1
 
