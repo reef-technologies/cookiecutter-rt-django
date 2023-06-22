72c18c66d2a97a093018c737f2d29a6972e6793e
Maciej Urbanski <maciej.urbanski+gh@reef.pl>
2023-06-22 15:40:24 +0200

    fix pytest django settings setting

--- {{cookiecutter.repostory_name}}/app/src/{{cookiecutter.django_project_name}}/{{cookiecutter.django_default_app_name}}/tests/settings.py
+++ {{cookiecutter.repostory_name}}/app/src/{{cookiecutter.django_project_name}}/{{cookiecutter.django_default_app_name}}/tests/settings.py
@@ -1,6 +1,10 @@
-from {{cookiecutter.django_project_name}}.settings import *  # noqa: F403
+import os
 
-DEBUG_TOOLBAR = False
-{%- if cookiecutter.monitoring == "y" %}
+os.environ.update({
+    "DEBUG_TOOLBAR": "False",
+})
+
+from {{cookiecutter.django_project_name}}.settings import *  # noqa: E402,F403
+{% if cookiecutter.monitoring == "y" %}
 PROMETHEUS_EXPORT_MIGRATIONS = False
 {%- endif %}
