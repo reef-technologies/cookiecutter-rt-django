a71a6885b7081a689729b25ac0cf56af5eec9421
Aleksandr Goncharov <aleksandr.goncharov@reef.pl>
2023-08-17 15:40:13 +0300

    Fix CELERY_TASK_SERIALIZER setting

--- {{cookiecutter.repostory_name}}/app/src/{{cookiecutter.django_project_name}}/settings.py
+++ {{cookiecutter.repostory_name}}/app/src/{{cookiecutter.django_project_name}}/settings.py
@@ -222,7 +222,7 @@ CELERY_TASK_ROUTES = ['{{cookiecutter.django_project_name}}.celery.route_task']
 CELERY_TASK_TIME_LIMIT = int(timedelta(minutes=5).total_seconds())
 CELERY_TASK_ALWAYS_EAGER = env.bool('CELERY_TASK_ALWAYS_EAGER', default=False)
 CELERY_ACCEPT_CONTENT = ['json']
-CELERY_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_WORKER_PREFETCH_MULTIPLIER = env.int('CELERY_WORKER_PREFETCH_MULTIPLIER', default=10)
 CELERY_BROKER_POOL_LIMIT = env.int('CELERY_BROKER_POOL_LIMIT', default=50)
