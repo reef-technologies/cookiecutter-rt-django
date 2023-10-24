cf73755411e79519663c62e582e73477d827f065
Maciej Urbanski <maciej.rooter.urbanski@reef.pl>
2023-10-24 17:29:37 +0200

    admin title

--- {{cookiecutter.repostory_name}}/app/src/{{cookiecutter.django_project_name}}/{{cookiecutter.django_default_app_name}}/admin.py
+++ {{cookiecutter.repostory_name}}/app/src/{{cookiecutter.django_project_name}}/{{cookiecutter.django_default_app_name}}/admin.py
@@ -1,2 +1,7 @@
 from django.contrib import admin  # noqa
 from django.contrib.admin import register  # noqa
+
+
+admin.site.site_header = "{{ cookiecutter.django_project_name }} Administration"
+admin.site.site_title = "{{ cookiecutter.django_project_name }}"
+admin.site.index_title = "Welcome to {{ cookiecutter.django_project_name }} Administration"
