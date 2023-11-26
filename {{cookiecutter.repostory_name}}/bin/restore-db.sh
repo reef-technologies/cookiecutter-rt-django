f944782eaa7225d1919ffd90a538bfdb3118083c
Maciej Urbanski <maciej.rooter.urbanski@reef.pl>
2023-11-26 15:44:47 +0100

    fix spellcheck erroring out on `cat` usage

--- {{cookiecutter.repostory_name}}/bin/restore-db.sh
+++ {{cookiecutter.repostory_name}}/bin/restore-db.sh
@@ -7,6 +7,7 @@ TARGET_FILEPATH="$1"
 
 DOCKER_NETWORK=$(get_db_docker_network)
 
+# shellcheck disable=SC2002
 cat "$TARGET_FILEPATH" | docker run -i --rm --network "$DOCKER_NETWORK" postgres:16-alpine pg_restore -c -d "$DATABASE_URL"
 
 echo 'restore finished'
