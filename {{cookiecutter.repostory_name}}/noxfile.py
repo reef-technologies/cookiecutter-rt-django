846855e7a2a3dc6abdae13852c19627e1e9359e1
Vykintas Baltrušaitis <vykintas.baltrusaitis@reef.pl>
2024-03-04 16:47:35 +0200

    Show diff when format check fails

--- {{cookiecutter.repostory_name}}/noxfile.py
+++ {{cookiecutter.repostory_name}}/noxfile.py
@@ -126,7 +126,7 @@ def lint(session):
     session.run("codespell", ".")
     run_shellcheck(session, mode="check")
     run_readable(session, mode="check")
-    session.run("ruff", "format", "--check", ".")
+    session.run("ruff", "format", "--diff", ".")
 
 
 @nox.session(python=PYTHON_DEFAULT_VERSION)
