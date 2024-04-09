1065496356de3a27eac1189b9fa0dc79b65c4faf
Adal Chiriliuc <adal.chiriliuc@reef.pl>
2024-04-09 23:44:36 +0300

    more robust setup.sh scripts

--- {{cookiecutter.repostory_name}}/setup-dev.sh
+++ {{cookiecutter.repostory_name}}/setup-dev.sh
@@ -1,25 +1,39 @@
 #!/usr/bin/env bash
 # Copyright 2017, Reef Technologies (reef.pl), All rights reserved.
 
+set -euo pipefail
+
 PROJECT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 ENV_DIR="./envs/dev"
 # shellcheck disable=SC2164
 cd "${PROJECT_DIR}"
 
-# Create a lock file, install Python dependencies
-[ -f pdm.lock ] || pdm lock --group :all
+# Workaround for PDM which sometimes creates a 3.10 venv
+if [[ ! -d ".venv" ]]; then
+    python3.11 -m venv .venv
+fi
+
+# Create a lock file if doesn't exist
+if [[ ! -f "pdm.lock" ]]; then
+    pdm lock --group :all
+fi
+# Install Python dependencies
 pdm sync --group :all
 
 # Create .env from the template if doesn't exist
-[[ -f "${ENV_DIR}/.env" ]] || cp "${ENV_DIR}/.env.template" "${ENV_DIR}/.env"
+if [[ ! -f "${ENV_DIR}/.env" ]]; then
+    cp "${ENV_DIR}/.env.template" "${ENV_DIR}/.env"
+fi
 
 # Set symlinks
 ln -sf "${ENV_DIR}/.env" .env
 ln -sf "${ENV_DIR}/docker-compose.yml" docker-compose.yml
+
 # shellcheck disable=SC2164
 cd "${PROJECT_DIR}/app/"
-[[ -L "Dockerfile" ]] && unlink Dockerfile
-[[ -L "src/entrypoint.sh" ]] && unlink src/entrypoint.sh
-
-# Ensure that the script returns zero for the CI
-exit 0
+if [[ -L "Dockerfile" ]]; then
+    unlink Dockerfile
+fi
+if [[ -L "src/entrypoint.sh" ]]; then
+    unlink src/entrypoint.sh
+fi
--- {{cookiecutter.repostory_name}}/setup-prod.sh
+++ {{cookiecutter.repostory_name}}/setup-prod.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 # Copyright 2017, Reef Technologies (reef.pl), All rights reserved.
 
+set -euo pipefail
+
 PROJECT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 ENV_DIR="./envs/prod"
 # shellcheck disable=SC2164
 cd "${PROJECT_DIR}"
 
 # Create .env from the template if doesn't exist
-[[ -f "${ENV_DIR}/.env" ]] || cp "${ENV_DIR}/.env.template" "${ENV_DIR}/.env"
+if [[ ! -f "${ENV_DIR}/.env" ]]; then
+    cp "${ENV_DIR}/.env.template" "${ENV_DIR}/.env"
+fi
 
 # Set symlinks
 ln -sf "${ENV_DIR}/.env" .env
