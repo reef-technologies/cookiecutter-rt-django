83f696968fa4dee02e2043c17f6d0f0a611741d6
Aleksandr Goncharov <aleksandr.goncharov@reef.pl>
2024-04-17 14:26:12 +0300

    Fix shell script globbing

--- {{cookiecutter.repostory_name}}/deploy-to-aws.sh
+++ {{cookiecutter.repostory_name}}/deploy-to-aws.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 # shellcheck disable=2086
-./devops/scripts/build-backend.sh $1
-./devops/scripts/deploy-backend.sh $1
+./devops/scripts/build-backend.sh "$1"
+./devops/scripts/deploy-backend.sh "$1"
