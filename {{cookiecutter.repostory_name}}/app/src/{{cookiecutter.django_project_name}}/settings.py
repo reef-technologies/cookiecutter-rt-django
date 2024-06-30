249ea476ec28b87401160e4cf7ed3527a0f31481
Kordian Kowalski <kordian.kowalski@reef.pl>
2024-06-30 14:38:05 +0200

    Attempt fix whitespace lint

--- {{cookiecutter.repostory_name}}/app/src/{{cookiecutter.django_project_name}}/settings.py
+++ {{cookiecutter.repostory_name}}/app/src/{{cookiecutter.django_project_name}}/settings.py
@@ -447,7 +447,6 @@ if SENTRY_DSN := env("SENTRY_DSN", default=""):
         ],
     )
     ignore_logger("django.security.DisallowedHost")
-
 {% if cookiecutter.use_allauth == "y" -%}
 LOGIN_REDIRECT_URL = "/"
 ACCOUNT_AUTHENTICATION_METHOD = "email"
