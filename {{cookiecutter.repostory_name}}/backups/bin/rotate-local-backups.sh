19a037155e8e6d741690fa3800fcce1eeedd19cd
Alex <aleksandr.goncharov@reef.pl>
2025-02-26 13:44:45 +0300

    Update {{cookiecutter.repostory_name}}/backups/bin/rotate-local-backups.sh
    Co-authored-by: emnoor-reef <137923473+emnoor-reef@users.noreply.github.com>

--- {{cookiecutter.repostory_name}}/backups/bin/rotate-local-backups.sh
+++ {{cookiecutter.repostory_name}}/backups/bin/rotate-local-backups.sh
@@ -2,7 +2,7 @@
 if [ -n "${BACKUP_LOCAL_ROTATE_KEEP_LAST:-}" ]; then
     echo "Rotating backup files - keeping ${BACKUP_LOCAL_ROTATE_KEEP_LAST} last ones"
     files_to_delete=$(find /var/backups -name "*.dump.zstd" | sort -r | tail -n "+${BACKUP_LOCAL_ROTATE_KEEP_LAST}")
-    echo "$files_to_delete" | xargs rm
+    echo "$files_to_delete" | xargs --no-run-if-empty rm
     echo "Removed:"
     echo "$files_to_delete"
 else
