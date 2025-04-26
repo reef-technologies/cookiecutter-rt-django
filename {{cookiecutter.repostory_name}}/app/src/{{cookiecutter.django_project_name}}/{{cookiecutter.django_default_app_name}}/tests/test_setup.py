7fb6240ba5d738a2d80ed483d60e4a348d93c1af
Olzhas Arystanov <olzhas.arystanov@reef.pl>
2025-04-26 20:32:13 +0700

    Introduce dedicated /alive/ endpoint for health checking

--- {{cookiecutter.repostory_name}}/app/src/{{cookiecutter.django_project_name}}/{{cookiecutter.django_default_app_name}}/tests/test_setup.py
+++ {{cookiecutter.repostory_name}}/app/src/{{cookiecutter.django_project_name}}/{{cookiecutter.django_default_app_name}}/tests/test_setup.py
@@ -1,6 +1,6 @@
 """
-This test file is here always to indicate that everything was installed and the CI was able to run tests.
-It should always pass as long as all dependencies are properly installed.
+This test file is here always to indicate that all dependencies were properly installed and the CI was able to run tests.
+It also verifies if the healthcheck endpoint is functioning correctly
 """
 
 from datetime import timedelta
@@ -16,3 +16,8 @@ def test__setup(db, some):
 
     with pytest.raises(ZeroDivisionError):
         1 / 0
+
+
+def test__alive_endpoint(client):
+    response = client.get("/alive/")
+    assert response.status_code == 200
