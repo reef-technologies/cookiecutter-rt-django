79f613750a471b68aef957cf650b3d773dcde79c
Grzegorz Leszczynski <grzegorz.leszczynski@reef.pl>
2025-10-14 12:21:15 +0200

    feat: Overwrite default RDS params with values better for performance

--- {{cookiecutter.repostory_name}}/devops/tf/main/modules/database/rds.tf
+++ {{cookiecutter.repostory_name}}/devops/tf/main/modules/database/rds.tf
@@ -15,12 +15,63 @@ resource "aws_db_subnet_group" "self" {
   }
 }
 
+resource "aws_db_parameter_group" "postgres_params" {
+  name_prefix   = "${var.name}-${var.env}-"
+  family = "postgres16"
+
+  parameter {
+    name         = "shared_buffers"
+    value        = "{DBInstanceClassMemory/32768}" # 1/4 of memory (value in 8kB blocks)
+  }
+
+  parameter {
+    name         = "effective_cache_size"
+    value        = "{DBInstanceClassMemory/16384}" # 1/2 of memory (value in 8kB blocks)
+    apply_method = "immediate"
+  }
+
+  parameter {
+    name         = "work_mem"
+    value        = "{DBInstanceClassMemory/65536}" # 1/64 of memory (value in 1kB blocks)
+    apply_method = "immediate"
+  }
+
+  parameter {
+    name         = "maintenance_work_mem"
+    value        = "{DBInstanceClassMemory/16384}" # 1/16 of memory (value in 1kB blocks)
+    apply_method = "immediate"
+  }
+
+  parameter {
+    name         = "autovacuum_work_mem"
+    value        = "{DBInstanceClassMemory/16384}" # 1/16 of memory (value in 1kB blocks)
+    apply_method = "immediate"
+  }
+
+  parameter {
+    name         = "effective_io_concurrency"
+    value        = "200"
+    apply_method = "immediate"
+  }
+
+  parameter {
+    name  = "random_page_cost"
+    value = "1.1"
+    apply_method = "immediate"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
 resource "aws_db_instance" "self" {
   identifier             = "${var.name}-${var.env}-db"
   allocated_storage      = 5
   max_allocated_storage  = 20
   storage_encrypted      = true
   engine                 = "postgres"
+  engine_version         = "16.8"
   instance_class         = var.instance_type
   username               = "master"
   db_name                = "backend"
@@ -29,6 +80,7 @@ resource "aws_db_instance" "self" {
   availability_zone      = var.azs[0]
   db_subnet_group_name   = aws_db_subnet_group.self.name
   vpc_security_group_ids = [aws_security_group.db.id]
+  parameter_group_name   = aws_db_parameter_group.postgres_params.name
 
   tags = {
     Project = var.name
