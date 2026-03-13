58da70ebddac15d3fa9d452f4bc5e1140f178efd
Aleksandr Goncharov <aleksandr.goncharov@reef.pl>
2026-03-13 23:21:53 +0300

    Don't suppress non-json log lines

--- {{cookiecutter.repostory_name}}/bin/logs.sh
+++ {{cookiecutter.repostory_name}}/bin/logs.sh
@@ -5,30 +5,31 @@
 
 set -eu
 docker compose logs --no-log-prefix "$@" | jq -Rr '
-  # process each line as JSON, skip if not valid JSON
-  fromjson? |
-  select(. != null) |
-  . as $orig |
+  . as $line |
+  try (
+    fromjson |
+    select(. != null) |
+    . as $orig |
 
-  # 1. Using standard ANSI indices (30-37, 90-97)
-  "\u001b[31m" as $red |      # Theme Red
-  "\u001b[32m" as $green |    # Theme Green
-  "\u001b[33m" as $yellow |   # Theme Yellow
-  "\u001b[34m" as $blue |     # Theme Blue
-  "\u001b[35m" as $purple |   # Theme Magenta/Purple
-  "\u001b[36m" as $cyan |     # Theme Cyan
-  "\u001b[90m" as $gray |     # Theme Bright Black / Gray
-  "\u001b[0m" as $reset |
+    "\u001b[31m" as $red |      # Theme Red
+    "\u001b[32m" as $green |    # Theme Green
+    "\u001b[33m" as $yellow |   # Theme Yellow
+    "\u001b[34m" as $blue |     # Theme Blue
+    "\u001b[35m" as $purple |   # Theme Magenta/Purple
+    "\u001b[36m" as $cyan |     # Theme Cyan
+    "\u001b[90m" as $gray |     # Theme Bright Black / Gray
+    "\u001b[0m" as $reset |
 
-  # select color based on level
-  (if $orig.level == "error" then $red
-   elif $orig.level == "warning" then $yellow
-   elif $orig.level == "info" then $green
-   else $gray end) as $lvl_color |
+    # select color based on level
+    (if $orig.level == "error" then $red
+    elif $orig.level == "warning" then $yellow
+    elif $orig.level == "info" then $green
+    else $gray end) as $lvl_color |
 
-  # form key=value pairs
-  del(.timestamp, .level, .event) |
-  to_entries | map("\($blue)\(.key)\($reset)=\($purple)\(.value)\($reset)") | join(" ") as $others |
+    # form key=value pairs
+    del(.timestamp, .level, .event) |
+    to_entries | map("\($blue)\(.key)\($reset)=\($purple)\(.value)\($reset)") | join(" ") as $others |
 
-  "\($gray)\($orig.timestamp)\($reset) [\($lvl_color)\($orig.level)\($reset)] \($orig.event) \($others)"
+    "\($gray)\($orig.timestamp)\($reset) [\($lvl_color)\($orig.level)\($reset)] \($orig.event) \($others)"
+  ) catch $line
 '
